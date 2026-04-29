8cfb582a48c6167380a918e053603cfbb87d9eed
mervan2013 <mervansivarooban2013@gmail.com>
2026-04-28 19:10:57 -0700

    Weather teller

---
+++ weathercondition.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print('The weather is pleasnt in', spring)
+    print('The weather is the same in', autumn)
+
+spring = "autumn"
+autumn = "spring"
+weather_condition()
