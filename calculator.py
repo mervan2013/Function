806d74365025882e74b73cdba40437be3353c000
mervan2013 <mervansivarooban2013@gmail.com>
2026-05-05 18:38:35 -0700

    circumfrece calculator

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def add(P, Q):
+    return P + Q
+def subtract(P, Q):
+    return P - Q
+def Multiply(P, Q):
+    return P * Q
+def divide(P, Q):
+    return P/Q
+
+print("Please select operation")
+print("a. Add")
+print("b. Subtract")
+print("c. Multiply")
+print("d. Divide")
+choice = input("Pleasd eneter choice dpending on what type of help you would like with type ratioal number enter one of the following (a/ b/ c/ d)")
+
+num_1 = int(input("Please enter your first number"))
+num_2 - int(input(Please enter your second number))
